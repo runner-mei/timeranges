5adc5be57643a75a7d9fa3696703340df7397c15
meifakun <runner.mei@gmail.com>
2021-11-29 19:41:48 +0800

    add more tests

--- timerangesub/timerangesub_test.go
+++ timerangesub/timerangesub_test.go
@@ -15,6 +15,41 @@ func TestTimeRangesSubtract(t *testing.T) {
 	}
 
 	subtractor = [][]int{{900, 930}}
+	minuend = [][]int{{830, 900}}
+	expectStr = "0900-0930"
+	if res := toStr(TimeRangesSubtract(subtractor, minuend)); res != expectStr {
+		t.Errorf("expect %s, but got %s", expectStr, res)
+	}
+
+	subtractor = [][]int{{900, 930}}
+	minuend = [][]int{{1030, 1100}}
+	expectStr = "0900-0930"
+	if res := toStr(TimeRangesSubtract(subtractor, minuend)); res != expectStr {
+		t.Errorf("expect %s, but got %s", expectStr, res)
+	}
+
+	subtractor = [][]int{{900, 930}}
+	minuend = [][]int{{830, 915}}
+	expectStr = "0915-0930"
+	if res := toStr(TimeRangesSubtract(subtractor, minuend)); res != expectStr {
+		t.Errorf("expect %s, but got %s", expectStr, res)
+	}
+
+	subtractor = [][]int{{900, 930}}
+	minuend = [][]int{{915, 1000}}
+	expectStr = "0900-0915"
+	if res := toStr(TimeRangesSubtract(subtractor, minuend)); res != expectStr {
+		t.Errorf("expect %s, but got %s", expectStr, res)
+	}
+
+	subtractor = [][]int{{900, 930}}
+	minuend = [][]int{{815, 1000}}
+	expectStr = ""
+	if res := toStr(TimeRangesSubtract(subtractor, minuend)); res != expectStr {
+		t.Errorf("expect %s, but got %s", expectStr, res)
+	}
+
+	subtractor = [][]int{{900, 930}}
 	minuend = [][]int{{930, 1500}}
 	expectStr = "0900-0930"
 	if res := toStr(TimeRangesSubtract(subtractor, minuend)); res != expectStr {
